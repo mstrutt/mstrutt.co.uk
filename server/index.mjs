4567956188cccec37fa1b4a681728ac8d390036e
Michael Strutt <michael@mstrutt.co.uk>
2019-05-26 22:42:30 +0100

    Reducing duplication

--- server/index.mjs
+++ server/index.mjs
@@ -13,90 +13,40 @@ const app = express();
 app.use(express.static('./dist'));
 
 app.get('/blog/', (req, res) => {
+  const pattern = /^\d+-\d+-.+\.json$/;
+
   readdirPromise('./blog')
     .then((files) => {
-      const pattern = /^\d+-\d+-.+\.json$/;
       const postList = files
         .filter(filename => filename.match(pattern))
         .sort()
-        .reverse()
-        .map(filename => readFilePromise(`./blog/${filename}`));
-      return Promise.all([
-        readFilePromise('./app/blog-listing.template.html'),
-        readFilePromise(`./dist/template.html`),
-        ...postList
-      ]);
+        .reverse();
+      return renderPostList(postList, title);
     })
-    .then(([blogListing, template, ...files]) => {
-      const filesJSON = files.map(file => JSON.parse(file));
-      const content = squirrelly.Render(blogListing, {
-        posts: filesJSON
-      });
-      const page = squirrelly.Render(template, {
-        title: 'Blog',
-        main: content
-      });
-      res.send(page);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.statusCode = 500;
-      res.send('500');
-    });
+    .then(page => res.send(page))
+    .catch(defaultCatch(res));
 });
 
 // Year and month search
 app.get('/blog/:year(\\d+)/(:month(\\d+)/)?', (req, res) => {
-  const {
-    year,
-    month
-  } = req.params;
-
+  const { year, month } = req.params;
   const pattern = new RegExp(`^${year || '\\d+'}-${month || '\\d+'}-.+\\.json$`);
-  let title = `Blog posts from ${year}`;
-  if (month) {
-    title += `/${month}`;
-  }
-
-  console.log(pattern);
+  const title = `Blog posts from ${year}${month ? `/${month}` : ''}`;
 
   readdirPromise('./blog')
     .then((files) => {
       const postList = files
         .filter(filename => filename.match(pattern))
         .sort()
-        .reverse()
-        .map(filename => readFilePromise(`./blog/${filename}`));
-      return Promise.all([
-        readFilePromise('./app/blog-listing.template.html'),
-        readFilePromise(`./dist/template.html`),
-        ...postList
-      ]);
+        .reverse();
+      return renderPostList(postList, title);
     })
-    .then(([blogListing, template, ...files]) => {
-      const filesJSON = files.map(file => JSON.parse(file));
-      const content = squirrelly.Render(blogListing, {
-        posts: filesJSON
-      });
-      const page = squirrelly.Render(template, {
-        title,
-        main: content
-      });
-      res.send(page);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.statusCode = 500;
-      res.send('500');
-    });
+    .then(page => res.send(page))
+    .catch(defaultCatch(res));
 });
 
 app.get('/blog/:year(\\d+)/:month(\\d+)/:slug([a-z0-9-]+)/', (req, res) => {
-  const {
-    year,
-    month,
-    slug
-  } = req.params;
+  const { year, month, slug } = req.params;
 
   Promise.all([
     readFilePromise(`./blog/${year}-${month}-${slug}.json`),
@@ -117,11 +67,35 @@ app.get('/blog/:year(\\d+)/:month(\\d+)/:slug([a-z0-9-]+)/', (req, res) => {
         res.statusCode = 404;
         res.send('404');
       } else {
-        console.error(err);
-        res.statusCode = 500;
-        res.send('500');
+        defaultCatch(res)(err)
       }
     });
 });
+
+function renderPostList(postList, title) {
+  return Promise.all([
+    readFilePromise('./app/blog-listing.template.html'),
+    readFilePromise(`./dist/template.html`),
+    ...postList.map(filename => readFilePromise(`./blog/${filename}`))
+  ])
+    .then(([listTemplate, pageTemplate, ...files]) => {
+      const filesJSON = files.map(file => JSON.parse(file));
+      const main = squirrelly.Render(listTemplate, {
+        posts: filesJSON
+      });
+      return squirrelly.Render(pageTemplate, {
+        title: title || 'Blog',
+        main
+      });
+    });
+}
+
+function defaultCatch(res) {
+  return (err) => {
+    console.error(err);
+    res.statusCode = 500;
+    res.send('500');
+  };
+}
 
 app.listen(8080);
