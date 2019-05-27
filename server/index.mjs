2ddbf357644943c6e98bbda3b3eac32ff483b3d0
Michael Strutt <michael@mstrutt.co.uk>
2019-05-27 10:37:53 +0100

    adding rss.xml handling

--- server/index.mjs
+++ server/index.mjs
@@ -92,6 +92,30 @@ router.get('/blog/:year(\\d+)/:month(\\d+)/:slug([a-z0-9-]+)/', (req, res) => {
     });
 });
 
+router.get('/rss.xml', (req, res) => {
+  readdirPromise('./blog')
+    .then((files) => {
+      const pattern = /^\d+-\d+-.+\.json$/;
+      const postList = files
+        .filter(filename => filename.match(pattern))
+        .sort()
+        .reverse();
+      return Promise.all([
+        readFilePromise('./app/rss.template.xml'),
+        ...postList.map(filename => readFilePromise(`./blog/${filename}`))
+      ]);
+    })
+    .then(([rssTemplate, ...files]) => {
+      const filesJSON = files.map(file => JSON.parse(file));
+      const page = squirrelly.Render(rssTemplate, {
+        posts: filesJSON
+      });
+      res.set('Content-Type', 'text/xml');
+      res.send(page);
+    })
+    .catch(defaultCatch(res));
+});
+
 Object.entries(redirects).forEach(([route, redirect]) => {
   router.get(route, (req, res) => {
     res
