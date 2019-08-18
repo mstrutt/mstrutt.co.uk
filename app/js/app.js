215cc8e7ee308513ef497c75190d0336a4ddd49a
Michael Strutt <michael@mstrutt.co.uk>
2019-08-18 20:56:19 +0100

    Styling homepage narrowest viewport

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,3 @@
 import ImageHandler from './classes/image-handler';
-import StickyAvatar from './classes/sticky-avatar';
 
 [...document.querySelectorAll('.blog-post img')].map(img => new ImageHandler(img));
-[...document.getElementsByClassName('avatar')].map(avatar => new StickyAvatar(avatar));
