b747f3847d6a312d7a555d55cfb295c97981b0d8
Michael Strutt <michael@mstrutt.co.uk>
2019-09-07 23:08:00 +0100

    Removing meta from logger

--- server/utils/logger.mjs
+++ server/utils/logger.mjs
@@ -3,7 +3,6 @@ import winston from 'winston';
 export default winston.createLogger({
   level: 'info',
   format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
   transports: process.env.NODE_ENV === 'production' ? [
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'combined.log' }),
